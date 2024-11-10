168e5791bf01c9b51bdd6072991ae777d2a4d714
Hyejeong <thswjd0609@gmail.com>
2024-11-11 02:47:16 +0900

    feat: InventoryService 클래스 생성

---
+++ src/main/java/store/service/InventoryService.java
@@ -0,0 +1,12 @@
+package store.service;
+
+import store.model.Inventory;
+
+public class InventoryService {
+    private Inventory inventory;
+
+    public InventoryService(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+}
