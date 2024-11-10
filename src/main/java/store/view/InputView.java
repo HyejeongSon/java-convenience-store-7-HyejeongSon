5df2e0256393ad08d85d0aeb77b31ab3293e92ce
Hyejeong <thswjd0609@gmail.com>
2024-11-11 07:39:42 +0900

    refactor: 반복 입력 가능하도록 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -10,23 +10,47 @@ public class InputView {
     }
 
     public static boolean askForAdditionalPurchase() {
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-        return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+        while (true) {
+            try {
+                System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+                return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static boolean askForMoreProduct(String productName, int quantity) {
-        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName, quantity);
-        return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+        while (true) {
+            try {
+                System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName, quantity);
+                return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static boolean askForFullPrice(String productName, int quantity) {
-        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", productName, quantity);
-        return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+        while (true) {
+            try {
+                System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", productName, quantity);
+                return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static boolean askMembershipDiscount() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+        while (true) {
+            try {
+                System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+                return validateResponse(Console.readLine()).equalsIgnoreCase("Y");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private static String validateResponse(String input) {
