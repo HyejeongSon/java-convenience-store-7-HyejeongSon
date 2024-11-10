73c29d934a374797a04a4bdf0625ae258948a6ea
Hyejeong <thswjd0609@gmail.com>
2024-11-11 03:32:42 +0900

    test: 인벤토리 로드 테스트

--- src/test/java/store/service/InventoryServiceTest.java
+++ src/test/java/store/service/InventoryServiceTest.java
@@ -1,4 +1,20 @@
+package store.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import store.model.Inventory;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class InventoryServiceTest {
-  
+
+    @Test
+    void 상품_프로모션_파일_입력_테스트() {
+        Inventory inventory = new Inventory();
+        InventoryService inventoryService = new InventoryService(inventory);
+        inventoryService.loadInventoryFromFile();
+        System.out.println(inventory);
+        Assertions.assertThat(inventory.productSize()).isEqualTo(11);
+        Assertions.assertThat(inventory.promotionSize()).isEqualTo(3);
+    }
 }
